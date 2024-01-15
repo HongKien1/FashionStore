5fc93563cbd3ef818f89269dee7c8466f16137d8
Elkjugking123 <tdao35738@gmail.com>
2024-01-15 13:50:09 +0700

    Entity Dao Dto

--- src/main/java/com/main/fashionstore/Entity/Cart.java
+++ src/main/java/com/main/fashionstore/Entity/Cart.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.repository.cdi.Eager;
+
 
 import java.io.Serializable;
 
