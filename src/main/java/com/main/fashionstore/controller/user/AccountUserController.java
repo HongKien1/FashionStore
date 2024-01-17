4b432064646a9f28c8a49cdac501926acf537ed5
Elkjugking123 <tdao35738@gmail.com>
2024-01-17 10:07:07 +0700

    Giao dien lich su mua hang thong tin tai khoan

--- src/main/java/com/main/fashionstore/controller/user/AccountUserController.java
+++ src/main/java/com/main/fashionstore/controller/user/AccountUserController.java
@@ -3,11 +3,12 @@ package com.main.fashionstore.controller.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
-@RequestMapping("orderDetail")
-public class OrderDetailController {
+@RequestMapping("account")
+public class AccountUserController {
     @GetMapping("")
     public String index() {
-        return "user/orderDetail";
+        return "user/account";
     }
 }
